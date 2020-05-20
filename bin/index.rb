9d755c0b5f2294f18662fb7e409e33f26ec528e6
Abdoulaye-Thespy <njigouhrazak@iut-dhaka.edu>
2020-05-20 12:26:42 +0100

    Added different folders and classes, added more logic to the logics file

--- bin/index.rb
+++ bin/index.rb
@@ -1,16 +1,57 @@
+#!/usr/bin/env ruby
+# frozen_string_literal: true
+
+require_relative '../lib/scraper.rb'
+require_relative '../lib/logic'
 require 'nokogiri'
 require 'httparty'
 require 'byebug'
 require 'pry'
-require_relative '../lib/logic.rb'
-require_relative '../lib/scraper.rb'
 
-obj = Scraper.new
-obj.scrape
-    job_obj=[]
+    obj = Scraper.new
+    obj.scrape
+    obj.create_hash
+    puts
+    puts
+    puts
 
-21.times do |x|
-    job_obj << {:location => obj.jobs[x].text, :job_title => obj.jobs[x].text, :salary => obj.jobs[x].text }
-	puts "Job location is #{obj.location[x]}" 
-end 
-Pry.start(binding)
+    puts ".............................Jobs got from page 1............................................"
+    job_obj = obj.hash
+    p job_obj
+loop do
+  puts
+  puts
+  puts
+  puts '................................choose page number, enter a number from 1 to 21.............................'
+  Page_num = gets.chomp.to_i
+  if Page_num > 1 && Page_num < 22
+    objf = Logic.new(Page_num)
+    20.times do |x|
+      puts 'Razak'
+      job_obj << { job_title: objf.get_job_title(x),
+                   location: objf.get_location(x),
+                   salary: objf.get_salary(x),
+                   employer: objf.get_employer(x),
+                   posted_date: objf.get_posted_date(x),
+                   link: objf.get_links(x) }
+    end
+    puts
+    puts
+    puts
+
+    puts ".............................Jobs got from page #{Page_num}............................................"
+
+
+  else puts '........................wrong number please enter a number from 1 to 21...................................'
+    end
+  puts job_obj
+  puts obj.hash
+  Pry.start(binding)
+end
+
+# CSV.open("jobs.csv", "wb") do |csv|
+#   obj.hash.each do |item|
+#    csv << [:, link]
+#   end
+# end
+# Pry.start(binding)
--- lib/logic.rb
+++ lib/logic.rb
@@ -1,4 +1,36 @@
+# frozen_string_literal: true
 
-class Logic
+require_relative './logic'
+class Logic < Scraper
+  attr_reader :newurl, :jobst
+  def initialize(x)
+    @newurl = "https://www.jobcoconut.com/job-search/page/#{x}"
+    @unparsed_page = HTTParty.get(newurl)
+    @parsed_page = Nokogiri::HTML(@unparsed_page)
+    @jobst = parsed_page.css('div.row')
+  end
 
+  def get_job_title(x)
+    @job_title = jobst[x].css('div.job-title').text.gsub("\n", ' ')
+  end
+
+  def get_location(x)
+    jobst[x].css('li.results-job-location').text
+  end
+
+  def get_salary(x)
+    jobst[x].css('li.results-salary').text
+  end
+
+  def get_employer(x)
+    jobst[x].css('li.results-client-name').text
+  end
+
+  def get_posted_date(x)
+    jobst[x].css('li.results-posted-at').text.gsub("\n", ' ')
+  end
+
+  def get_links(x)
+    jobst[x].css('a').attr('href').value
+  end
 end
